e74ca9714a1ee9b0e685be8d146bc72813f53f00
vigneshs1997 <vigneshmaster1997@gmail.com>
2026-03-30 13:14:16 +0530

    Initial gemini-chat

---
+++ src/test/java/com/spvm/gemini_chat/GeminiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spvm.gemini_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
